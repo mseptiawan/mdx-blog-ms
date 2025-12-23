b6cf6f7e9fe2b31b33f57c4375704c5be61ca9a7
mseptiawan <mseptiawan017@gmai.com>
2025-12-23 16:32:59 +0700

    afasd

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export const dynamic = 'force-dynamic'
 
 export default function BlogPage() {
-  // 🔑 WAJIB ADA
+  // 🔑 WAJIB AD
   const categories = getCategories()
 
   return (
